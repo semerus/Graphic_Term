b210320779e7b44b693c38a1472c0c8de3f3e8ce
semerus <semerus4u@gmail.com>
2016-05-17 00:17:19 +0900

    button test, first-person test, GridManager progressing

---
+++ Maze_Shoot_src/Assets/Script/GridManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour {
+
+	public Vector3 wallPosition;
+	public GameObject wallPrefab;
+	//private int x = 10;
+	//private int y = 5;
+	//private ArrayList = new ArrayList();
+	private int[,] _grid = new int[,] {
+		{1, 1, 1, 1, 1, 1, 0, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 1, 0, 1, 1, 1, 0, 1},
+		{1, 1, 1, 1, 0, 1, 0, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 1, 1, 1, 0, 1, 0, 1},
+		{1, 0, 0, 0, 0, 1, 2, 1, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+
+	// Use this for initialization
+	void Start () {
+		//Instantiate (wallPrefab, wallPosition, Quaternion.identity);
+		Vector3 position;
+		for (int i; i<10; i++)
+		{
+			for (int j; j<10; j++)
+			{
+				if(_grid[i,j] == 1)
+				{
+					position.Set(GridConverter(i,j));
+					Instantiate(wallPrefab, position, Quaternion.identity);
+				}
+			}
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private Vector3 GridConverter(int x, int y)
+	{
+		Vector3 position;
+		float vx, vz;
+		vx = -4.5f + (float)y;
+		vz = -4.5f + (float)x;
+		position.Set (vx, 0.5f, vz);
+		return position;
+	}
+}
+
+/* maze structure 0=space, 1=wall, 2=playerstart
+{1, 1, 1, 1, 1, 1, 0, 1, 1, 1}
+{1, 0, 0, 0, 0, 0, 0, 1, 0, 1}
+{1, 0, 1, 1, 0, 1, 1, 1, 0, 1}
+{1, 1, 1, 1, 0, 1, 0, 1, 0, 1}
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+{1, 0, 1, 1, 1, 1, 1, 1, 1, 1}
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+{1, 0, 1, 1, 1, 1, 0, 1, 0, 1}
+{1, 0, 0, 0, 0, 1, 2, 1, 0, 1}
+{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+*/
---
+++ Maze_Shoot_src/Assets/Script/UserControl.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UserControl : MonoBehaviour {
+
+	Transform playerTrans;
+	Vector3 playerPosition;
+	public Vector3 road;
+	public Vector3 step;
+	public Vector3 stepX;
+	public int rotateSpeed;
+	private Vector3 walk;
+	private Vector3 walkCount;
+	private bool go;
+	private int forward = 3;
+	private bool rotate = false;
+	// Use this for initialization
+	void Start () {
+		playerTrans = GetComponent<Transform>();
+		playerPosition = playerTrans.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (forward == 2) {
+			if (walkCount.z < walk.z) {
+				playerTrans.Translate (step, Space.World);
+				walkCount += step;
+			}
+			if (walkCount.z >= walk.z) {
+				walk[2] = 0;
+				walkCount[2] = 0;
+				forward = 1;
+			}
+		}
+		if(forward == 1)
+		{
+			if(rotate == false)
+			{
+				playerTrans.Rotate(0, 90, 0);
+				rotate = true;
+			}
+			if(walkCount.x < walk.x)
+			{
+				playerTrans.Translate (stepX, Space.World);
+				walkCount += stepX;
+			}
+			if (walkCount.x >= walk.x) {
+				walk[0] = 0;
+				walkCount.Set (0, 0, 0);
+				forward = 3;
+			}
+
+
+		}
+	}
+	public void MoveUp(bool click)
+	{
+		//playerTrans.Translate (road, Space.World);
+		//playerTrans.Rotate (0, 90, 0);
+		walk = road;
+		forward = 2;
+	}
+}
