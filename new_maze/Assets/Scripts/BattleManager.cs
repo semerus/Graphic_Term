3d638633457780c8cb9069b223facf6d39e7484b
semerus <semerus4u@gmail.com>
2016-06-21 14:27:31 +0900

    added player model

--- new_maze/Assets/Scripts/BattleManager.cs
+++ new_maze/Assets/Scripts/BattleManager.cs
@@ -6,10 +6,13 @@ public class BattleManager : MonoBehaviour {
 
 	GameObject crossarrow;
 	public float speed;
+	private float fireRate = 0.5f;
+	private float nextFire = 0.0f;
 	Vector2 up = new Vector2(0f, 1f);
 	Vector2 down = new Vector2(0f, -1f);
 	Vector2 right = new Vector2(1f, 0f);
 	Vector2 left = new Vector2(-1f, 0f);
+
 	// Use this for initialization
 	void Start () {
 		crossarrow = GameObject.FindWithTag ("CrossArrow");
@@ -26,21 +29,22 @@ public class BattleManager : MonoBehaviour {
 			crossarrow.GetComponent<RectTransform> ().anchoredPosition += right * speed;
 		if (Input.GetKey ("left"))
 			crossarrow.GetComponent<RectTransform> ().anchoredPosition += left * speed;
-		if (Input.GetKey ("c")) {
+		if (Input.GetKey ("c") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
 			Debug.Log ("hit!");
 			//Vector3 aim = Vector2 (crossarrow.GetComponent<RectTransform> ().anchoredPosition);
 			//Vector2 aim = Camera.current.WorldToScreenPoint(crossarrow.GetComponent<RectTransform> ().anchoredPosition);
 			//Ray ray = Camera.main.ScreenPointToRay (crossarrow.);
 			//Ray ray = crossarrow.GetComponent<RectTransform> ().anchoredPosition;
-			Ray ray = Camera.current.ScreenPointToRay(Input.mousePosition);
+
 			//Ray ray = Camera.current.ScreenPointToRay(crossarrow.GetComponent<RectTransform> ().anchoredPosition);
+			Ray ray = Camera.current.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hitInfo;
 			if(Physics.Raycast (ray, out hitInfo, 10f)) {
-				Debug.DrawRay (ray.origin, ray.direction * 10, Color.green);
 				Debug.Log ("info" + hitInfo.collider.name);
 				if (hitInfo.collider.tag == "Monster") {
 					GameObject target = GameObject.Find (hitInfo.collider.name);
-					target.gameObject.SetActive (false);
+					target.GetComponent<MonsterStatus>().HealthDecrease();
 				}
 			}
 		}
---
+++ new_maze/Assets/Scripts/MonsterStatus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterStatus : MonoBehaviour {
+	private int _health = 100;
+	private int _damage = 50;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_health < 1) {
+			this.gameObject.SetActive (false);
+		}
+	}
+
+	public void HealthDecrease() {
+		_health = _health - _damage;
+	}
+}
